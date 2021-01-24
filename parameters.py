6d0573a4d35cc4834f3dcd8d348a8d7ff28b32e7
WillCPP <WillCPP@users.noreply.github.com>
2021-01-24 14:45:28 -0500

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,11 @@
+parameters = {
+    'transfer_model_emnist' : {
+        'learning_rate' : 0.001,
+        'noise_multiplier' : 0.1,
+        'l2_norm_clip' : 1.0,
+        'num_layers' : 5,
+        'batch_size' : 128,
+        'epochs' : 15,
+        'validation_split' : 0.1
+    }
+}
